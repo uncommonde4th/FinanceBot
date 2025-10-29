14c9ecd781eddfe0762a338a603f700635e0cca5
George Brezhnev <uncommonde4th@gmail.com>
2025-10-29 12:11:05 +0300

    v0.1

--- bot.py
+++ bot.py
@@ -11,3 +11,34 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден, проверьте .env")
+
+def load_messages():
+    try:
+        with open('data/messages.json', 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("message.json не найден")
+        return {}
+    except json.JSONDecodeError:
+        print("Ошибка в формате messages.json")
+        return {}
+
+MESSAGES = load_messages()
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    text = MESSAGES.get('start_message', 'Сообщение не найдено')
+    bot.reply_to(message, text, parse_mode='Markdown')
+
+
+if __name__ == '__main__':
+    print("Successfully started")
+    try:
+        bot.polling(none_stop=True, interval=0)
+    except Exception as e:
+        print(f'Error: {e}')
